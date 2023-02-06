1d8d2a1f35d3b3645a62e41d203f4bb887d76c37
nighfy <82936194+nighfy@users.noreply.github.com>
2023-02-06 12:18:30 +0100

    Add a dummy board and a fuction to display the current state

--- javascript.js
+++ javascript.js
@@ -1,3 +1,38 @@
 /* eslint-disable strict */
 
 'use strict';
+
+const gameboard = (() => {
+  let board = [
+    ['X', 'O', 'X'],
+    ['O', 'X', 'O'],
+    ['X', 'O', 'X'],
+  ];
+  const getBoard = () => board;
+  return { getBoard };
+})();
+
+const displayController = (() => {
+  const boardNode = document.querySelector('div.gameboard');
+  const updateBoard = (boardState) => {
+    console.dir(boardNode);
+    console.table(boardState);
+    for (let r = 1; r <= 3; r += 1) {
+      for (let i = 1; i <= 3; i += 1) {
+        const square = document.querySelector(`#s${r}${i}`);
+        square.textContent = boardState[r - 1][i - 1];
+        console.log(square);
+      }
+    }
+  };
+  return { updateBoard };
+})();
+
+const game = (() => {
+  const init = () => {
+    displayController.updateBoard(gameboard.getBoard());
+  };
+  return { init };
+})();
+
+game.init();
