d54d7fd9b7d9c8fef64f5ff3badff87b74bd7b4b
nighfy <82936194+nighfy@users.noreply.github.com>
2023-02-07 18:58:04 +0100

    Add function checking if game is over

--- javascript.js
+++ javascript.js
@@ -47,11 +47,47 @@ const game = (() => {
       playerSign = playerSign === 'O' ? 'X' : 'O';
     };
 
+    const checkState = () => {
+      const board = Object.values(gameboard.getBoard());
+      for (let r = 0; r < 3; r += 1)
+        if (
+          board[r][0] !== '' &&
+          board[r][0] === board[r][1] &&
+          board[r][0] === board[r][2]
+        ) {
+          return board[r][0];
+        }
+      for (let c = 0; c < 3; c += 1)
+        if (
+          board[0][c] !== '' &&
+          board[0][c] === board[1][c] &&
+          board[0][c] === board[2][c]
+        ) {
+          return board[0][c];
+        }
+      if (
+        board[0][0] !== '' &&
+        board[0][0] === board[1][1] &&
+        board[0][0] === board[2][2]
+      ) {
+        return board[0][0];
+      }
+      if (
+        board[2][0] !== '' &&
+        board[2][0] === board[1][1] &&
+        board[2][0] === board[0][2]
+      ) {
+        return board[2][0];
+      }
+      return 'continue';
+    };
+
     const squares = document.querySelectorAll('.square');
     Object.values(squares).forEach((square) => {
       square.addEventListener('click', (e) => {
         const { id } = e.target;
         if (gameboard.setSquare(id, playerSign) !== 'taken') {
+          console.log(checkState());
           changePlayer();
         }
       });
